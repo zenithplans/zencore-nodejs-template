a737247015c2d9b120c29b81b92ff917eafacf11
Aneesh SK <aneeshsk1996@gmail.com>
2024-06-23 01:09:55 -0400

    refactor(infra): move server config to centralized infra config

---
+++ src/infrastructure/infraConfig.ts
@@ -0,0 +1,7 @@
+const serverConfig = {
+	PENDING_CONNECTION_QUEUE_LENGTH: 511,
+	LISTENING_PORT: 42069,
+	LISTENING_HOST: "0.0.0.0",
+} as const;
+
+export { serverConfig };
--- src/infrastructure/server/index.ts
+++ src/infrastructure/server/index.ts
@@ -1,4 +1,6 @@
 import { createServer } from "node:http";
+import { serverConfig } from "../infraConfig";
+
 import type { Server, ServerOptions } from "node:http";
 
 export default class ApplicationServer {
@@ -21,19 +23,13 @@ export default class ApplicationServer {
 
 	private initializeErrorHandler() {
 		// TODO: exit process gracefully.
-		// TODO: setup notification/error logging and monitoring for server failures
+		// TODO: setup error logging and monitoring for server failures
 		this._server.on("error", (error) => {
 			console.error("[CRITICAL] Encountered unhandled server failure, further investigation needed:::", error);
 		});
 	}
 
 	public listen() {
-		const serverConfig = {
-			PENDING_CONNECTION_QUEUE_LENGTH: 511,
-			LISTENING_PORT: 42069,
-			LISTENING_HOST: "0.0.0.0",
-		} as const;
-
 		this._server.listen(
 			serverConfig.LISTENING_PORT,
 			serverConfig.LISTENING_HOST,
